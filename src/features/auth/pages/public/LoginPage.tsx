36f568457c59dd1fca710ce86ede7c26216b7047
jonathan <jonathan114311@gmail.com>
2026-08-06 16:40:28 -0500

    implements public routes

---
+++ src/features/auth/pages/public/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function LoginPage() {
+  return (
+    <>
+      <h1>LoginPage</h1>
+      <Link to="/register">register</Link>
+    </>
+  );
+}
+
+export default LoginPage;
---
+++ src/features/auth/pages/public/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function RegisterPage() {
+  return (
+    <>
+      <h1>RegisterPage</h1>;<Link to="/">login</Link>
+    </>
+  );
+}
+
+export default RegisterPage;
