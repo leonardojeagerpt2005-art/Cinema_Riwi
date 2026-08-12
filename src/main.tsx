ea0be84210aa0377ae88aae0aa1a622c633966cb
Leonardo Perez <leonardojeagerpt2005@gmail.com>
2026-08-12 08:00:13 -0500

    Revert "Feature/register"

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import { router } from "./appRouter";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
-);
+)
